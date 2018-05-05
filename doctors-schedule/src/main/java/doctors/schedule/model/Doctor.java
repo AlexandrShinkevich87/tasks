bb1b061acbf360e7c5faa7a8b1e24a2132940de0
alexandr.shinkevich <alexandr.shinkevich@gmail.com>
2018-05-06 01:00:05 +0600

    task 01

--- doctors-schedule/src/main/java/doctors/schedule/model/Doctor.java
+++ doctors-schedule/src/main/java/doctors/schedule/model/Doctor.java
@@ -1,10 +1,15 @@
 package doctors.schedule.model;
 
 import java.time.DayOfWeek;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Doctor {
     private String fullName;
     private Map<DayOfWeek, List<TimeSlot>> schedule;
+
+    private List<TimeSlot> sort(Collection<TimeSlot> timeSlots) {
+        List<TimeSlot> result = new ArrayList<>(timeSlots);
+        Collections.sort(result, TimeSlot::compareTo);
+        return result;
+    }
 }
