35dc66db1fe96b9eee8600a8c7ee1b35b1b04882
alexandr.shinkevich <alexandr.shinkevich@gmail.com>
2018-05-07 20:41:13 +0600

    task 01

--- doctors-schedule/src/main/java/doctors/schedule/model/Doctor.java
+++ doctors-schedule/src/main/java/doctors/schedule/model/Doctor.java
@@ -5,8 +5,10 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.time.DayOfWeek;
-import java.time.LocalTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @NoArgsConstructor
 @Data
@@ -18,12 +20,6 @@ public class Doctor {
      */
     private Map<String, List<TimeSlot>> schedule = new HashMap<>();
 
-    private List<TimeSlot> sort(Collection<TimeSlot> timeSlots) {
-        List<TimeSlot> result = new ArrayList<>(timeSlots);
-        Collections.sort(result, TimeSlot::compareTo);
-        return result;
-    }
-
     //['ИВАНОВ ИВАН ИВАНОВИЧ', '12:00-16:00', '12:00-16:00', '12:00-16:00', '12:00-16:00', '12:00-16:00', '12:00-16:00', ''],
     //['ИВАНОВ ИВАН ИВАНОВИЧ', '08:00-12:00', '08:00-10:00', '08:00-16:00', '', '', '', ''],
     public Map<String, List<TimeSlot>> mergeSchedule(Map<String, List<TimeSlot>> schedule) {
@@ -34,7 +30,7 @@ public class Doctor {
             List<TimeSlot> timeSlots = entry.getValue();
 
             List<TimeSlot> mergedTimeSlot =
-                    mergeTimeSlot(
+                    TimeSlot.merge(
                             new ArrayList<TimeSlot>() {{
                                 if (!timeSlots.contains(null)) {
                                     addAll(timeSlots);
@@ -48,39 +44,5 @@ public class Doctor {
             System.out.println(entry.getKey() + "/" + entry.getValue());
         }
         return mergedSchedule;
-    }
-
-    private List<TimeSlot> mergeTimeSlot(List<TimeSlot> intervalTimeSlots) {
-        if (intervalTimeSlots.size() <= 1) {
-            return intervalTimeSlots;
-        }
-        List<TimeSlot> intervals = sort(intervalTimeSlots);
-
-        TimeSlot pastInterval = intervals.get(0);
-
-        List<TimeSlot> mergedInterval = new ArrayList<>();
-
-        for (int i = 1; i < intervals.size(); i++) {
-            TimeSlot currentInterval = intervals.get(i);
-
-            // if the current time of slot overlaps with the last time of slot, use the later end time of the two
-            // currentInterval.start <= pastInterval.end
-            if (currentInterval.getStart().isBefore(pastInterval.getEnd()) || currentInterval.getStart().equals(pastInterval.getEnd())) {
-                /*// if the first interval can be merged with the currentInterval interval
-                // end = max(currentInterval.end, end);
-                end = currentInterval.getEnd().isAfter(end) ? currentInterval.getEnd() : end;*/
-                if (currentInterval.getEnd().isAfter(pastInterval.getEnd())) {
-                    // this means currentInterval finishes outside of the past-intervals limit
-                    TimeSlot newInterval = new TimeSlot(pastInterval.getStart(), currentInterval.getEnd());
-                    pastInterval = newInterval;
-                }
-            } else {
-                // as the past interval cannot be merged within the current interval, its the beginning of new interval
-                mergedInterval.add(pastInterval);
-                pastInterval = currentInterval;
-            }
-        }
-        mergedInterval.add(pastInterval);
-        return mergedInterval;
     }
 }
--- doctors-schedule/src/main/java/doctors/schedule/model/TimeSlot.java
+++ doctors-schedule/src/main/java/doctors/schedule/model/TimeSlot.java
@@ -2,6 +2,10 @@ package doctors.schedule.model;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class TimeSlot {
 
@@ -9,7 +13,6 @@ public class TimeSlot {
 
     private final LocalTime start;
     private final LocalTime end;
-
 
     public TimeSlot(LocalTime start, LocalTime end) {
         if (start.isAfter(end)) {
@@ -34,6 +37,46 @@ public class TimeSlot {
 
     int compareTo(TimeSlot timeSlot) {
         return start.compareTo(timeSlot.start);
+    }
+
+    private static List<TimeSlot> sort(Collection<TimeSlot> timeSlots) {
+        List<TimeSlot> result = new ArrayList<>(timeSlots);
+        Collections.sort(result, TimeSlot::compareTo);
+        return result;
+    }
+
+    public static List<TimeSlot> merge(List<TimeSlot> intervalTimeSlots) {
+        if (intervalTimeSlots.size() <= 1) {
+            return intervalTimeSlots;
+        }
+        List<TimeSlot> intervals = sort(intervalTimeSlots);
+
+        TimeSlot pastInterval = intervals.get(0);
+
+        List<TimeSlot> mergedInterval = new ArrayList<>();
+
+        for (int i = 1; i < intervals.size(); i++) {
+            TimeSlot currentInterval = intervals.get(i);
+
+            // if the current time of slot overlaps with the last time of slot, use the later end time of the two
+            // currentInterval.start <= pastInterval.end
+            if (currentInterval.getStart().isBefore(pastInterval.getEnd()) || currentInterval.getStart().equals(pastInterval.getEnd())) {
+                /*// if the first interval can be merged with the currentInterval interval
+                // end = max(currentInterval.end, end);
+                end = currentInterval.getEnd().isAfter(end) ? currentInterval.getEnd() : end;*/
+                if (currentInterval.getEnd().isAfter(pastInterval.getEnd())) {
+                    // this means currentInterval finishes outside of the past-intervals limit
+                    TimeSlot newInterval = new TimeSlot(pastInterval.getStart(), currentInterval.getEnd());
+                    pastInterval = newInterval;
+                }
+            } else {
+                // as the past interval cannot be merged within the current interval, its the beginning of new interval
+                mergedInterval.add(pastInterval);
+                pastInterval = currentInterval;
+            }
+        }
+        mergedInterval.add(pastInterval);
+        return mergedInterval;
     }
 
     public LocalTime getStart() {
